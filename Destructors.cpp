f30f982b9e75223e07db52057493d4ea89d77b2c
Premkumar Patil <premkumarpatil093@gmail.com>
2025-02-04 20:43:14 +0530

    Add files via upload

---
+++ Destructors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Test 
+{
+	public:
+	Test() { cout << "\n Constructor executed"; }
+	~Test() { cout << "\nDestructor executed"; }
+};
+	main()
+{
+	Test t;
+	return 0;
+}
